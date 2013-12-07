e8f69817cf6db351e3154d78c45d70093e53eab0
Etienne Millon <me@emillon.org>
2013-12-07 19:16:49 +0100

    Imported Upstream version 1.0.2

--- lib/register_plugins.c
+++ lib/register_plugins.c
@@ -77,10 +77,11 @@ extern MetaDataSource lyrics_lyricsvip_src;
 extern MetaDataSource lyrics_lyricswiki_src;
 extern MetaDataSource lyrics_lyrix_src;
 extern MetaDataSource lyrics_magistrix_src;
-extern MetaDataSource lyrics_metrolyrics_src;
+//extern MetaDataSource lyrics_metrolyrics_src;
 extern MetaDataSource lyrics_metallum_src;
 extern MetaDataSource lyrics_elyrics_src;
 extern MetaDataSource lyrics_chartlyrics_src;
+extern MetaDataSource lyrics_vagalume_src;
 extern MetaDataSource photos_flickr_src;
 extern MetaDataSource photos_google_src;
 extern MetaDataSource photos_lastfm_src;
@@ -177,9 +178,10 @@ static void register_provider_plugins (void)
     plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_lyricswiki_src);
     plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_lyrix_src);
     plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_magistrix_src);
-    plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_metrolyrics_src);
+    //plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_metrolyrics_src);
     plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_metallum_src);
     plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_elyrics_src);
+	plugin_add_to_list (&glyrMetaDataSourceList,&lyrics_vagalume_src);
 
     // FIXME: Chartlyrics reacts very slowly, and often not at all
     // add again when running again ( :( - good api though)
